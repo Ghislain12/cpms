e6c6977b51a0de6a77086672ff0473ed78c2565e
Gael AMOUSSOU <gael-ghislain.amoussou@epitech.eu>
2023-02-04 12:23:47 +0100

    commit

---
+++ laravel/resources/views/layouts/base.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @hasSection('title')
+        <title>@yield('title') - {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="{{ asset('css/inter.css') }}">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+    @livewireStyles
+
+    <!-- Scripts -->
+    <script src="{{ url(mix('js/app.js')) }}" defer></script>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="{{ asset('css/tailwind.min.css') }}" rel="stylesheet">
+    <style>
+        @import url({'https://fonts.googleapis.com/css?family=Karla:400,700&display=swap'});
+
+        .font-family-karla {
+            font-family: karla;
+        }
+
+        .bg-sidebar {
+            background: #3d68ff;
+        }
+
+        .cta-btn {
+            color: #3d68ff;
+        }
+
+        .upgrade-btn {
+            background: #1947ee;
+        }
+
+        .upgrade-btn:hover {
+            background: #0038fd;
+        }
+
+        .active-nav-link {
+            background: #1947ee;
+        }
+
+        .nav-item:hover {
+            background: #1947ee;
+        }
+
+        .account-link:hover {
+            background: #3d68ff;
+        }
+    </style>
+</head>
+
+<body>
+    @yield('body')
+
+    @livewireScripts
+</body>
+
+</html>
